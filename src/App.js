5339430e2ff4ff0ed42a86c12ce42bebc43efed8
rajeshprema <rajeshprema774@gmail.com>
2023-11-06 19:14:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Pricecard from './Components/Pricecard/pricecard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Pricecard/>
+    </div>
+  );
+}
+
+export default App;
